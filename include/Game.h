76a4fa8f0f6177f25d7974556bb4f9b10bd6af31
Jeff DiMarco <jdimarco218@gmail.com>
2015-11-18 21:52:28 -0500

    Updating Makefile

---
+++ include/Game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+
+class Game
+{
+public:
+
+private:
+    vector<Player> _playerList;
+    Shoe _shoe;
+    
+    :set shiftwidt=4
+    
+
+};
+
+#endif
